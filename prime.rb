ace73e2bd7c06952716e1b8f3d7b2171983d61e2
cd <coraldarling@outlook.com>
2018-09-07 15:08:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,8 +6,8 @@ def prime?(i)
     true
   elsif
     array = (2..(i/2)).to_a
-    #new_array = []
-    new_array = array.each do |number| 
+    new_array = []
+    array.each do |number| 
       new_array.push(i % number)
     end
       if new_array.include?(0) == true
