737d00e0fb41a5d22fbe8fcb7d720a662f6febe0
cd <coraldarling@outlook.com>
2018-09-07 15:04:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(i)
+  if i <= 1 || ((i.even? == true) && i != 2)
+    false
+  elsif
+    (i = 3) || (i = 2) 
+    true
+  elsif
+    array = (2..(i/2)).to_a
+    #new_array = []
+    new_array = array.each do |number| 
+      new_array.push(i % number)
+    end
+      if new_array.include?(0) == true
+          false 
+      else
+        true
+      end
+  end  
+end
