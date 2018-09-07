227492543a687817c96801bf8742efd853a0f244
cd <coraldarling@outlook.com>
2018-09-07 15:21:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(i)
+  if i <= 1 || ((i.even? == true) && i != 2)
+    false
+  elsif
+    (i == 3) || (i == 2) 
+    true
+  else
+    array = (2..(i/2)).to_a
+    new_array = []
+    array.each do |number| 
+      new_array.push(i % number)
+    end
+    
+      if new_array.include?(0) == true
+        return false 
+      else
+        return true
+      end
+  end  
+end
