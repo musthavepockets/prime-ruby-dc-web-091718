64e4966ac209cccf5be92e633bf30069fbae8137
cd <coraldarling@outlook.com>
2018-09-07 15:20:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,10 @@
-require 'pry'
-
 def prime?(i)
   if i <= 1 || ((i.even? == true) && i != 2)
     false
   elsif
     (i == 3) || (i == 2) 
     true
-  elsif
+  else
     array = (2..(i/2)).to_a
     new_array = []
     array.each do |number| 
